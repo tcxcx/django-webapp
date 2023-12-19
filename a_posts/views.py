fd1f49c7c9b5b8699ae891867c9b0746958dac3f
Tomas Cordero <tomas.cordero.esp@gmail.com>
2023-12-19 18:31:35 -0500

    feat: views create posts

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,4 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.forms import ModelForm
+from django import forms
 
 def home_view (request): 
-    return render(request, 'a_posts/home.html')
+    posts = Post.objects.all()
+    return render(request, 'a_posts/home.html', {'posts' : posts})
+
+class PostCreateForm(ModelForm) :
+    class Meta:
+        model = Post
+        fields = '__all__'
+        labels = {
+            'body' : 'Caption',
+        }
+        widgets = {
+            'body' : forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a caption ...', 'class': 'font1 text-4xl'}),
+        }
+
+def post_create_view(request):
+    form = PostCreateForm()
+    
+    if request.method == 'POST': 
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    return render(request, 'a_posts/post_create.html', {'form' :  form })
