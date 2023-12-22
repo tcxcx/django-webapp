791b2957152a320d32185274170332852bf18871
Tomas Cordero <tomas.cordero.esp@gmail.com>
2023-12-22 16:50:52 -0500

    feat: added signals to events

--- a_users/views.py
+++ a_users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 from django.contrib.auth import logout
 from django.contrib import messages
 from django.http import Http404
@@ -26,7 +27,13 @@ def profile_edit_view(request):
         if form.is_valid():
             form.save()
             return redirect('profile')
-    return render(request, 'a_users/profile_edit.html', {'form' : form})
+        
+    if request.path == reverse('profile-onboarding'):
+        template = 'a_users/profile-onboarding.html'
+    else:
+        template = 'a_users/profile-profile_edit.html'
+
+    return render(request, template, {'form' : form})
 
 @login_required
 def profile_delete_view(request):
